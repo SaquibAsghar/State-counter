391f6725c62dfa5b2f473a5dc3f17ff2fa3004a8
SaquibAsghar <saq9516@gmail.com>
2020-05-12 00:48:52 +0530

    Counter
    In this app, I created an counter app using hooks. In this tertiary coundition is applied on the( increment/ decrement) button to not reach beyound the maxima/minima value, and a reset button to set the current counter value to 0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header>
+				<h1>Counter app using State/Hooks</h1>
+			</header>
+			<h2>Current value of count is {counter}</h2>
+			<button onClick={() => setCounter(0)}>Reset Counter</button>
+			<button onClick={() => counter >= 10? null: setCounter(counter + 1)}>Increase Counter</button>
+			<button onClick={() => counter <= 0 ?null :setCounter(counter - 1)}>Decrease Counter</button>
+		</div>
+	);
 }
 
 export default App;
